f18449052700f293ab56eec29e385671ba834331
Manash Kumar Mandal <manashmndl@gmail.com>
2016-06-06 13:36:02 +0600

    fetching is going smoothly

--- Sandbox/LoginThread.py
+++ Sandbox/LoginThread.py
@@ -2,6 +2,7 @@ from PyQt4 import QtGui
 from PyQt4.QtCore import QThread, SIGNAL
 from selenium import webdriver
 import time
+import re
 
 import sys
 
@@ -14,7 +15,7 @@ class LoginThread(QThread):
 
         #library url
         self.libraryUrl = 'http://library.kuet.ac.bd'
-
+        self.loggedIn = False
 
 
     def __del__(self):
@@ -25,7 +26,12 @@ class LoginThread(QThread):
         self.emit(SIGNAL('update_status(QString)'), 'Trying to log in ... be patient')
 
         # browser
+        # Use phantomjs for release
+        #self.libraryBrowser = webdriver.PhantomJS('C:\Users\Manash\Downloads\Compressed\phantomjs\phantomjs.exe')
+
+        # Use Chrome as default engine
         self.libraryBrowser = webdriver.Chrome()
+
         self.libraryBrowser.get(self.libraryUrl)
         time.sleep(1)
 
@@ -37,8 +43,29 @@ class LoginThread(QThread):
 
         self.libraryBrowser.find_element_by_class_name('submit').submit()
 
+        web_element = self.libraryBrowser.find_elements_by_tag_name('p')
+
+        self.loggedIn = not (True in ['incorrect' in item.text for item in web_element])
+
+        if not self.loggedIn:
+            self.emit(SIGNAL('update_status(QString)'), 'ID or password is incorrect, try again')
+            self.libraryBrowser.close()
+        else:
+            self.emit(SIGNAL('update_status(QString)'), 'Successfully logged in')
+            self.get_book_data()
+
+
+    def get_book_data(self):
+        parent_tbody = self.libraryBrowser.find_element_by_tag_name('tbody')
+        child_tr = parent_tbody.find_elements_by_tag_name('tr')
+        
+
+        for item in child_tr:
+            print item.text
+
+
+
     def run(self):
         self.check_login()
-        self.emit(SIGNAL('log_in(QString)'), 'logging in')
         self.sleep(2)
 
--- Sandbox/login.py
+++ Sandbox/login.py
@@ -2,6 +2,7 @@ import sys
 from PyQt4 import uic, QtGui
 from PyQt4.QtCore import SIGNAL
 from LoginThread import LoginThread
+import ast
 
 #Login design path
 ui_login = 'design/ui_login.ui'
@@ -23,29 +24,44 @@ class Login(QtGui.QWidget, Ui_LoginWidget):
         self.IDLineEdit.setText('051203043')
         self.passwordLineEdit.setText('051203043')
 
+        # Button
+
     def init_ui(self):
         self.loginPushButton.clicked.connect(self.check_login)
+        self.nextPushButton.setEnabled(False)
 
 
     def check_login(self):
         self.ID = str(self.IDLineEdit.text())
         self.password = str(self.passwordLineEdit.text())
 
-        #Debugging purpose
-        print self.ID, self.password
-
+        # Creating thread instance
         self.loginThread = LoginThread(self.ID, self.password)
         self.loginThread.start()
-        self.connect(self.loginThread, SIGNAL("log_in(QString)"), self.clogin)
-        self.connect(self.loginThread, SIGNAL('update_status(QString)'), self.update_status)
 
-    def clogin(self, text):
-        print str(text)
+        # Connecting the thread
+        self.connect(self.loginThread, SIGNAL('update_status(QString)'), self.update_status)
+        self.connect(self.loginThread, SIGNAL('fetch_book_data(QString)'), self.fetch_book_data)
 
     def update_status(self, status):
         self.status = str(status)
         self.statusLabel.setText(self.status)
 
+        #Enable next on successful login
+        if "Successfully logged" in self.status:
+            self.nextPushButton.setEnabled(True)
+        elif "incorrect" in self.status:
+            self.nextPushButton.setEnabled(False)
+
+
+
+    def fetch_book_data(self, info):
+        """
+        TODO:
+        Get's all info on logging in to library
+        """
+        info_dict = ast.literal_eval(str(info))
+        print info_dict
 
 
 if __name__ == '__main__':
